d215a39cb0ba3b33c140181c15a966e6c062bfe3
Davide <davidedipierro@outlook.it>
2023-12-28 22:24:42 +0100

    I clienti entrano ed escono

---
+++ Client/cliente.c
@@ -0,0 +1,67 @@
+/*
+ * Client che si connette al server e invia una richiesta
+ * e stampa la risposta
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <semaphore.h>
+
+#define MAX_REQUEST_SIZE 1024
+#define MAX_RESPONSE_SIZE 1024
+
+void send_request(int socket, char * request);
+void read_response(int socket, char * response);
+
+int main(int argc, char** argv) {
+    while (1){
+    
+        int sockfd;
+        struct sockaddr_in server_address;
+
+        // Create socket
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("Socket creation failed");
+            exit(1);
+        }
+
+        // Set server address
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(8080);
+        server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+        // Connect to server
+        if (connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+            perror("Connection failed");
+            exit(1);
+        }
+
+        char request[MAX_REQUEST_SIZE];
+        char response[MAX_RESPONSE_SIZE];
+
+        printf("Inserisci la richiesta: ");
+        fgets(request, MAX_REQUEST_SIZE, stdin);
+        send_request(sockfd, request);
+        read_response(sockfd, response);
+        printf("Risposta: %s\n", response);
+
+        close(sockfd);
+    }
+    return 0;
+}
+
+void send_request(int socket, char * request) {
+    if(write(socket, request, strlen(request)) == -1) perror("Write"), exit(1);
+}
+
+void read_response(int socket, char * response) {
+    if(read(socket, response, MAX_RESPONSE_SIZE) == -1) perror("Read"), exit(1);
+}
+
--- Server/server.c
+++ Server/server.c
@@ -31,6 +31,7 @@ typedef struct {
 
 void* process(void * ptr);
 void send_response(int socket, char * response);
+void read_request(int socket, char * request);
 
 carrello_t carrelli[MAX_CLIENTI];
 
@@ -43,6 +44,7 @@ pthread_mutex_t mutex_cassieri = PTHREAD_MUTEX_INITIALIZER;
 int n_cassieri = 0;
 
 int main(int argc, char * argv[]) {
+    printf("Starting server\n");
     int i = 0;
     int server_socket = 0;
     int client_socket = 0;
@@ -51,17 +53,15 @@ int main(int argc, char * argv[]) {
     sem_init(&sem_clienti, 0, MAX_CLIENTI);
 
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) perror("Could not create socket"), exit(EXIT_FAILURE);
-
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(PORT);
 
     // Collego il socket all'indirizzo e alla porta specificati
     if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) perror("Could not bind socket"), exit(EXIT_FAILURE);
-
     // Ascolto le connessioni in entrata
     if (listen(server_socket, MAX_CONNECTIONS) < 0) perror("Could not listen on socket"), exit(EXIT_FAILURE);
-
+    printf("Server started\nWaiting for connections...\n");
     while (1) {
         client_socket = accept(server_socket, NULL, NULL);
 
@@ -82,13 +82,13 @@ void* process(void * ptr) {
     char response[MAX_RESPONSE_SIZE];
 
     // Leggo la richiesta del client
-    read(socket, request, MAX_REQUEST_SIZE);
+    read_request(socket, request);
 
     // Processo la richiesta
-    if(strstr(request, "cliente entra") == 0) clienteEntra(request, response);
-    else if(strstr(request, "cliente esce") == 0) clienteEsce(request, response);
-    else if(strstr(request, "cassiere entra") == 0) cassiereEntra(request, response);
-    else if(strstr(request, "cassiere esce") == 0) cassiereEsce(request, response);
+    if(strstr(request, "cliente entra") != NULL) clienteEntra(request, response);
+    else if(strstr(request, "cliente esce") != NULL) clienteEsce(request, response);
+    else if(strstr(request, "cassiere entra") != NULL) cassiereEntra(request, response);
+    else if(strstr(request, "cassiere esce") != NULL) cassiereEsce(request, response);
     else printf("Richiesta non valida\n"); 
     
 
@@ -100,37 +100,47 @@ void* process(void * ptr) {
 }
 
 void send_response(int socket, char* response) {
-    write(socket, response, strlen(response));
+    if(write(socket, response, strlen(response)) == -1) perror("Write"), exit(1);
 }
 
-//sscanf(stringa, "Il numero è: %d", &numero);
+void read_request(int socket, char* request) {
+    if(read(socket, request, MAX_REQUEST_SIZE) == -1) perror("Read"), exit(1);
+}
 
 void clienteEntra(char* request, char* response){
     sem_wait(&sem_clienti);
     pthread_mutex_lock(&mutex_n_clienti);
     n_clienti++;
-    printf("Cliente entrato, clienti in negozio: %d\n", n_clienti);    
     pthread_mutex_unlock(&mutex_n_clienti);
+    int val;
+    sem_getvalue(&sem_clienti, &val);
+    printf("Cliente entrato, clienti in negozio: %d\n", val);
+    strcpy(response, "Sei entrato nel negozio\n\0"); 
 }
 
 void clienteEsce(char* request, char* response){
     pthread_mutex_lock(&mutex_n_clienti);
     n_clienti--;
-    printf("Cliente uscito, clienti in negozio: %d\n", n_clienti);
     pthread_mutex_unlock(&mutex_n_clienti);
     sem_post(&sem_clienti);
+    int val;
+    sem_getvalue(&sem_clienti, &val);
+    printf("Cliente uscito, clienti in negozio: %d\n", val);
+    strcpy(response, "Sei uscito dal negozio\n\0");
 }
 
 void cassiereEntra(char* request, char* response){
     pthread_mutex_lock(&mutex_cassieri);
     n_cassieri++;
-    printf("Cassiere entrato, cassieri in negozio: %d\n", n_cassieri);
     pthread_mutex_unlock(&mutex_cassieri);
+    printf("Cassiere entrato, cassieri in negozio: %d\n", n_cassieri);
+    strcpy(response, "Sei entrato nel negozio\n\0");
 }
 
 void cassiereEsce(char* request, char* response){
     pthread_mutex_lock(&mutex_cassieri);
     n_cassieri--;
-    printf("Cassiere uscito, cassieri in negozio: %d\n", n_cassieri);
     pthread_mutex_unlock(&mutex_cassieri);
+    printf("Cassiere uscito, cassieri in negozio: %d\n", n_cassieri);
+    strcpy(response, "Sei uscito dal negozio\n\0");
 }   
