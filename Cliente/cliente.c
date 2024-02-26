e3ed5cc225175690164f3e8459934e68a30c7521
Davide <davidedipierro@outlook.it>
2024-02-26 12:41:58 +0100

    m

--- Cliente/cliente.c
+++ Cliente/cliente.c
@@ -87,7 +87,7 @@ int create_socket(){
     // Set server address
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(5050);
-    server_address.sin_addr.s_addr = inet_addr("192.168.1.254");
+    server_address.sin_addr.s_addr = inet_addr("13.39.85.223");
 
     // Connect to server
     if (connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
