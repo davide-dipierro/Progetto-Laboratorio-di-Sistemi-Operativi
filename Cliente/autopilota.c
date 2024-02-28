43d306e2e12eb532e1c8ff56fe97b392d79f1650
Davide <davidedipierro@outlook.it>
2024-02-28 08:58:02 +0100

    Rimossi file e modificato indirizzo IP del server

--- Cliente/autopilota.c
+++ Cliente/autopilota.c
@@ -57,7 +57,7 @@ int create_socket(){
     // Set server address
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(5050);
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_address.sin_addr.s_addr = inet_addr("172.20.0.2");
 
     // Connect to server
     if (connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
--- Cliente/autopilota.sh
+++ Cliente/autopilota.sh
@@ -2,10 +2,8 @@
 
 for i in $(seq 1 $1);
 do
-	#waitTime=$(awk 'BEGIN { srand(); printf("%.1f\n", rand() * 1.9 + 0.1) }')
-	#echo "Aspetto $waitTime secondi"
-	#sleep $waitTime
-	$(./a.out > log$i)&
+	sleep 0.1
+	$(./autopilota > log$i)&
 done
 
 wait
